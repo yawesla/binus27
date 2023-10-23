f38589cde23384d6e686c331f61eebd934f29f8d
Irfan Zidny <125230318+yawesla@users.noreply.github.com>
2023-10-23 04:38:24 +0200

    Update LC-AngleClock.c

--- semester1/UTS/done/LC-AngleClock.c
+++ semester1/UTS/done/LC-AngleClock.c
@@ -20,15 +20,43 @@
 // }
 
 //impove
+// #include <stdio.h>
+// #include <math.h>
+
+// int main() {
+//   int hour, minute,test_case;
+//   scanf("%d", &test_case);
+
+//   for(int i=0;i<test_case;i++){
+
+//   scanf("%d %d", &hour, &minute);
+
+//   double h = (hour % 12 * 30) + (minute / 60 * 30);
+//   double m = minute * 6;
+
+//   double angle = abs(h - m);
+
+//   if (angle > 180) {
+//     angle = 360.0 - angle;
+//   }
+
+//   printf("Angle between hour and minute hand: %.2f degrees\n", angle);
+//   }
+
+  
+
+//   return 0;
+// }
+
+
+
+//onlingdb
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+
 
 int main() {
   int hour, minute,test_case;
-  scanf("%d", &test_case);
-
-  for(int i=0;i<test_case;i++){
-
   scanf("%d %d", &hour, &minute);
 
   double h = (hour % 12 * 30) + (minute / 60 * 30);
@@ -41,9 +69,7 @@ int main() {
   }
 
   printf("Angle between hour and minute hand: %.2f degrees\n", angle);
-  }
 
-  
 
   return 0;
 }
