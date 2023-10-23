18a629d43016d8967d728faf6e72911eff50ee6a
Irfan Zidny <125230318+yawesla@users.noreply.github.com>
2023-10-23 06:50:53 +0200

    Update Bard-Password.c

--- semester1/UTS/done/Bard-Password.c
+++ semester1/UTS/done/Bard-Password.c
@@ -14,18 +14,20 @@
 // "Panjangnya minimal 8 karakter."
 // "Mengandung setidaknya satu huruf besar, satu huruf kecil, satu angka, dan satu karakter khusus."
 // "Tidak mengandung kata-kata yang umum digunakan."
-
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 
 int main(){
     char password[256];
     int upper=0,lower=0,number=0,special=0,isLooping=0;
 
-    scanf("%s",&password);
+    scanf("%s", password);
+    // printf("%s",password);
 
     int len = strlen(password);
     for(int i=0;i<len;i++){
+        // printf("sebelum %c sesudah %c\n",password[i],password[i+1]);
         if(isupper(password[i])){
             upper++;
         } else if(islower(password[i])){
@@ -36,11 +38,14 @@ int main(){
             special++;
         }
     
+        if( password[i+1] == password[i]){
+            isLooping++;
+        }
     }
 
 
-    printf("total upper:%d, lower: %d, number:%d, Special:%d isLopping:%d\n",upper,lower,number,special);
-    if(len>8 && upper && lower && number && special){
+    printf("total char: %d upper:%d, lower: %d, number:%d, Special:%d isLooping:%d\n",len,upper,lower,number,special,isLooping);
+    if(len>8 && upper && lower && number && special && isLooping == 0) {
         printf("Password Kuat\n");
     } else {
         printf("Password Lemah\n");
