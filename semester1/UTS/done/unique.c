78c1a5e2eb55778eb7230954c12b0d3445475934
Irfan Zidny <125230318+yawesla@users.noreply.github.com>
2023-10-23 07:32:56 +0200

    Update unique.c

--- semester1/UTS/done/unique.c
+++ semester1/UTS/done/unique.c
@@ -18,27 +18,45 @@
 
 // Sample Output (2) :
 // not unique
-
 #include <stdio.h>
 
-int main(){
-    int tc =2;
-    int x,y;
-    int num1,num2;
+int main() {
+    int x, y;
 
-    scanf("%d %d",&x,&y);
+    // Assuming the maximum value of elements is 100
+    int temp[101] = {0};
+    int temp2[101] = {0};
 
-    for(int i=0;i<tc;i++){
-        
-        for(int j=0;j < x;j++){
-            scanf("%d",&num1);
-            printf("print %d ",num1);
-        }
+    scanf("%d %d", &x, &y);
 
+    // Read and populate temp array
+    for (int j = 0; j < x; j++) {
+        int num1;
+        scanf("%d", &num1);
+        temp[num1] = 1; // Mark the element as present
+    }
 
-        for(int k=1;k < y;k++){
-            scanf("%d",&num2);
-            printf("print %d ",num2);
+    // Read and populate temp2 array
+    for (int k = 0; k < y; k++) {
+        int num2;
+        scanf("%d", &num2);
+        temp2[num2] = 1; // Mark the element as present
+    }
+
+    int isUnique = 0; // Assuming unique initially
+
+    for (int i = 1; i <= x+y; i++) {
+        if (temp[i] == temp2[i]) {
+            isUnique = 1; // If any element differs, it's not unique
+            break;
         }
     }
+
+    if (isUnique == 0) {
+        printf("Unique\n");
+    } else {
+        printf("Not Unique\n");
+    }
+
+    return 0;
 }
